aea54a4aa0a945e34d304d3f4189844234a1ce33
EC2 Default User <ec2-user@ip-172-31-69-20.ec2.internal>
2021-11-24 23:25:56 +0000

    [add]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,6 +25,21 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to post_images_path
+  end
+
+
+
   private
   # ストロングパラメータ
   def book_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,15 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+  # ストロングパラメータ
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 
 end
